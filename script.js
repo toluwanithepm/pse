bef4fe7a33c7087b8797faffd6019b727d89aa54
toluwanithepm <johndoe@example.com>
2023-06-23 01:12:22 +0100

    changed from parallax to carousel

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
-function updateParallax() {
-  var scrollTop = window.scrollY || window.pageYOffset;
-  var layers = document.getElementsByClassName('parallax-layer');
-  
-  for (var i = 0; i < layers.length; i++) {
-    var layer = layers[i];
-    var speed = parseFloat(layer.getAttribute('data-speed'));
-    var yPos = -(scrollTop * speed);
-    
-    layer.style.transform = 'translate3d(0px, ' + yPos + 'px, 0px)';
-  }
+var carouselInner = document.querySelector('.carousel-inner');
+var carouselItems = document.querySelectorAll('.carousel-item');
+var currentIndex = 0;
+var isTransitioning = false;
+
+function goToSlide(index) {
+  if (isTransitioning) return;
+
+  carouselInner.style.transform = 'translateX(' + (-index * 100) + '%)';
+  currentIndex = index;
+  isTransitioning = true;
+
+  setTimeout(function() {
+    isTransitioning = false;
+  }, 500);
+
+  updateActiveState();
 }
 
-window.addEventListener('scroll', function() {
-  window.requestAnimationFrame(updateParallax);
+function goToNextSlide() {
+  var nextIndex = (currentIndex + 1) % carouselItems.length;
+  goToSlide(nextIndex);
+}
+
+function goToPrevSlide() {
+  var prevIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  goToSlide(prevIndex);
+}
+
+function updateActiveState() {
+  carouselItems.forEach(function(item, index) {
+    if (index === currentIndex) {
+      item.classList.add('active');
+      item.classList.remove('inactive');
+    } else {
+      item.classList.add('inactive');
+      item.classList.remove('active');
+    }
+  });
+}
+
+document.querySelector('.carousel-prev').addEventListener('click', function(event) {
+  event.preventDefault();
+  goToPrevSlide();
+});
+
+document.querySelector('.carousel-next').addEventListener('click', function(event) {
+  event.preventDefault();
+  goToNextSlide();
+});
+
+var touchStartX = 0;
+var touchEndX = 0;
+var swipeThreshold = 50;
+
+document.addEventListener('touchstart', function(event) {
+  touchStartX = event.touches[0].clientX;
+});
+
+document.addEventListener('touchend', function(event) {
+  touchEndX = event.changedTouches[0].clientX;
+  
+  if (touchStartX - touchEndX > swipeThreshold) {
+    goToNextSlide();
+  } else if (touchEndX - touchStartX > swipeThreshold) {
+    goToPrevSlide();
+  }
 });
